15173fe931baf241dbb4edf49abca7c75310614b
Facundo Dalessio <fdalessio22@gmail.com>
2024-05-07 17:36:55 -0300

    lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,14 +9,24 @@ import { MayorMenorComponent } from './componentes/mayor-menor/mayor-menor.compo
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: "full" },
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'registro', component: RegistroComponent },
-    { path: 'quienSoy', component: QuienSoyComponent },
-    { path: 'ahorcado', component: AhorcadoComponent,
+    { path: 'home',
+        loadComponent: () => import('./componentes/home/home.component').then(c => c.HomeComponent) 
+    },
+    { path: 'login',
+        loadComponent: () => import('./componentes/login/login.component').then(c => c.LoginComponent) 
+    },
+    { path: 'registro',
+        loadComponent: () => import('./componentes/registro/registro.component').then(c => c.RegistroComponent)
+    },
+    { path: 'quienSoy',
+        loadComponent: () => import('./componentes/quien-soy/quien-soy.component').then(c => c.QuienSoyComponent)
+    },
+    { path: 'ahorcado',
+        loadComponent: () => import('./componentes/ahorcado/ahorcado.component').then(c => c.AhorcadoComponent),
         ...canActivate(()=> redirectUnauthorizedTo(['/login']))
     },
-    { path: 'mayorMenor', component: MayorMenorComponent,
-    ...canActivate(()=> redirectUnauthorizedTo(['/login']))
+    { path: 'mayorMenor',
+        loadComponent: () => import('./componentes/mayor-menor/mayor-menor.component').then(c => c.MayorMenorComponent),
+        ...canActivate(()=> redirectUnauthorizedTo(['/login']))
     }
 ];
